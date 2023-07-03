740a8af8b474c566a8ff5db15c0acfd2fe5ddec4
Le Corre <jibeguitare@hotmail.fr>
2023-07-03 13:39:13 +0200

    🚧 wip(displayContent): dynamically add content into home page html thru javascript object

--- src/app/content.js
+++ src/app/content.js
@@ -1,57 +1,77 @@
+const name = "Jean-Baptiste&#32;Le&nbsp;Corre";
+const prez = [
+  {
+    content:
+      "I am a <strong>software developer, sound operator</strong>, studying computer science at 42 school.",
+  },
+  {
+    content:
+      "Passionate and curious about tech. Exploring and digging several fields of computer science (system, cybersecurity, devops, data science, web).",
+  },
+];
+const social = [
+  {
+    class: "tooltip",
+    dataTooltip: "github",
+    href: "https://github.com/Jibus22",
+    target: "_blank",
+    icon: "devicon-github-original",
+  },
+  {
+    class: "tooltip",
+    dataTooltip: "linkedin",
+    href: "https://www.linkedin.com/in/jle-corr/",
+    target: "_blank",
+    icon: "icon-linkedin-with-circle",
+  },
+  {
+    class: "tooltip",
+    dataTooltip: "mail",
+    href: "mailto:jeanbaptistelc@outlook.fr",
+    target: "_blank",
+    icon: "icon-envelope-o",
+  },
+  {
+    class: "tooltip",
+    dataTooltip: "Download resume",
+    href: "public/JBLC_EN_RESUME.pdf",
+    target: "_blank",
+    icon: "icon-file-text2",
+  },
+];
+
 export const displayContent = () => {
+  let displayPrez = prez.map((item) => {
+    return `
+        <div class="prez">
+          ${item.content}
+        </div>
+    `;
+  });
+  displayPrez = displayPrez.join("");
+  let displaySocial = social.map((item) => {
+    return `
+      <li>
+        <a
+          class=${item.class}
+          data-tooltip=${item.dataTooltip}
+          href=${item.href}
+          target=${item.target}
+        >
+          <span class=${item.icon}></span>
+        </a>
+      </li>
+    `;
+  });
+  displaySocial = displaySocial.join("");
+
   const content = `
     <div class="page-container">
       <div class="page-content" id="home">
-        <h1>Jean-Baptiste&#32;Le&nbsp;Corre</h1>
-        <div class="prez">
-          I am a <strong>software developer, sound operator</strong>, studying
-          computer science at 42 school.
-        </div>
-        <div class="prez">
-          Passionate and curious about tech. Exploring and digging several
-          fields of computer science (system, cybersecurity, devops, data
-          science, web).
-        </div>
+        <h1>${name}</h1>
+        ${displayPrez}
         <ul class="social">
-          <li>
-            <a
-              class="tooltip"
-              data-tooltip="github"
-              href="https://github.com/Jibus22"
-              target="_blank"
-            >
-              <span class="devicon-github-original"></span>
-            </a>
-          </li>
-          <li>
-            <a
-              class="tooltip"
-              data-tooltip="linkedin"
-              href="https://www.linkedin.com/in/jle-corr/"
-              target="_blank"
-            >
-              <span class="icon-linkedin-with-circle"></span>
-            </a>
-          </li>
-          <li>
-            <a
-              class="tooltip"
-              data-tooltip="mail"
-              href="mailto:jeanbaptistelc@outlook.fr"
-            >
-              <span class="icon-envelope-o"></span>
-            </a>
-          </li>
-          <li>
-            <a
-              class="tooltip"
-              data-tooltip="Download resume"
-              href="public/JBLC_EN_RESUME.pdf"
-              target="_blank"
-            >
-              <span class="icon-file-text2"></span>
-            </a>
-          </li>
+          ${displaySocial}
         </ul>
       </div>
     </div>
