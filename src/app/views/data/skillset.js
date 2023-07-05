7dffaa392ff31bd3b8a694e8b9a63c2d31fe02bc
Le Corre <jibeguitare@hotmail.fr>
2023-07-05 12:54:27 +0200

    🚧 wip(data): add data to skillset view

--- src/app/views/data/skillset.js
+++ src/app/views/data/skillset.js
@@ -1,23 +1,198 @@
 export const skills = [
   {
-    icon: "devicon-html5-plain",
-    title: "html",
-    subtitle: "web development",
-    category: "programming",
+    icon: "devicon-c-plain",
+    title: "C",
+    subtitle: "programming",
+    category: ["programming", "system"],
   },
   {
     icon: "devicon-cplusplus-plain",
     title: "C++",
     subtitle: "programming",
-    category: "programming",
+    category: ["programming", "system"],
+  },
+  {
+    icon: "icon-cogs",
+    title: "Atmega328p",
+    subtitle: "programming",
+    category: ["programming", "system"],
+  },
+  {
+    icon: "devicon-python-plain",
+    title: "Python3",
+    subtitle: "programming",
+    category: ["programming", "devops", "datascience"],
+  },
+  {
+    icon: "icon-code",
+    title: "Asm",
+    subtitle: "programming",
+    category: ["programming", "system"],
+  },
+  {
+    icon: "devicon-html5-plain",
+    title: "Html",
+    subtitle: "web development",
+    category: ["programming", "web"],
+  },
+  {
+    icon: "devicon-css3-plain",
+    title: "CSS",
+    subtitle: "web development",
+    category: ["programming", "web"],
+  },
+  {
+    icon: "devicon-javascript-plain",
+    title: "Javascript",
+    subtitle: "web development",
+    category: ["programming", "web"],
   },
   {
     icon: "devicon-nodejs-plain",
     title: "Node.js",
     subtitle: "web development",
-    category: "programming",
+    category: ["programming", "web"],
+  },
+  {
+    icon: "devicon-nestjs-plain",
+    title: "Nestjs",
+    subtitle: "web development",
+    category: ["programming", "web"],
+  },
+  {
+    icon: "devicon-react-original",
+    title: "Reactjs",
+    subtitle: "web development",
+    category: ["programming", "web"],
+  },
+  {
+    icon: "devicon-git-plain",
+    title: "Git",
+    subtitle: "development",
+    category: ["programming", "tools & environment", "devops"],
+  },
+  {
+    icon: "devicon-github-plain",
+    title: "Github",
+    subtitle: "development",
+    category: ["programming", "tools & environment", "devops"],
+  },
+  {
+    icon: "devicon-slack-plain",
+    title: "Slack",
+    subtitle: "development",
+    category: ["programming", "tools & environment", "devops"],
+  },
+  {
+    icon: "icon-code",
+    title: "Miro",
+    subtitle: "development",
+    category: ["programming", "tools & environment", "devops"],
+  },
+  {
+    icon: "devicon-docker-plain",
+    title: "Docker",
+    subtitle: "development",
+    category: ["programming", "tools & environment", "devops"],
+  },
+  {
+    icon: "devicon-kubernetes-plain",
+    title: "Kubernetes",
+    subtitle: "development",
+    category: ["programming", "devops"],
+  },
+  {
+    icon: "devicon-nginx-plain",
+    title: "Nginx",
+    subtitle: "development",
+    category: ["programming", "web", "devops"],
+  },
+  {
+    icon: "icon-database",
+    title: "SQL",
+    subtitle: "development",
+    category: ["programming", "web", "datascience"],
+  },
+  {
+    icon: "devicon-postgresql-plain",
+    title: "Postgresql",
+    subtitle: "development",
+    category: ["programming", "web", "datascience"],
+  },
+  {
+    icon: "icon-wrench",
+    title: "Gdb",
+    subtitle: "development",
+    category: ["programming", "tools & environment"],
+  },
+  {
+    icon: "icon-code",
+    title: "Make",
+    subtitle: "development",
+    category: ["programming", "tools & environment"],
+  },
+  {
+    icon: "devicon-bash-plain",
+    title: "Bash",
+    subtitle: "development",
+    category: ["programming", "devops"],
+  },
+  {
+    icon: "devicon-linux-plain",
+    title: "Linux",
+    subtitle: "development",
+    category: ["programming", "tools & environment"],
+  },
+  {
+    icon: "devicon-numpy-plain",
+    title: "Numpy",
+    subtitle: "development",
+    category: ["programming", "datascience"],
+  },
+  {
+    icon: "devicon-pandas-plain",
+    title: "Pandas",
+    subtitle: "development",
+    category: ["programming", "datascience"],
+  },
+  {
+    icon: "icon-stats-dots",
+    title: "matplotlib",
+    subtitle: "development",
+    category: ["programming", "datascience"],
+  },
+  {
+    icon: "devicon-vim-plain",
+    title: "Vim",
+    subtitle: "development",
+    category: ["programming", "tools & environment"],
+  },
+  {
+    icon: "icon-bar_chart",
+    title: "Protools",
+    subtitle: "post-production",
+    category: ["audio"],
+  },
+  {
+    icon: "icon-bar_chart",
+    title: "Yamaha 01V96i",
+    subtitle: "mixing console",
+    category: ["audio"],
+  },
+  {
+    icon: "icon-bar_chart",
+    title: "Behringer x32",
+    subtitle: "mixing console",
+    category: ["audio"],
+  },
+  {
+    icon: "icon-bar_chart",
+    title: "Riedel Bolero",
+    subtitle: "intercom system",
+    category: ["audio"],
   },
 ];
+
 export const filterButtons = [
   {
     icon: "icon-flash",
@@ -28,7 +203,27 @@ export const filterButtons = [
     text: "Programming",
   },
   {
-    icon: "icon-music",
+    icon: "icon-gear",
+    text: "system",
+  },
+  {
+    icon: "icon-globe",
+    text: "web",
+  },
+  {
+    icon: "icon-cloud",
+    text: "devops",
+  },
+  {
+    icon: "icon-pie-chart",
+    text: "datascience",
+  },
+  {
+    icon: "icon-stack",
+    text: "tools & environment",
+  },
+  {
+    icon: "icon-bar_chart",
     text: "Audio",
   },
 ];
